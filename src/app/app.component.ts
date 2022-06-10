bc7a5769b7c7de43e268bda3780de8723b2dcdb6
alejandro <nestoraca.179@gmail.com>
2022-06-10 16:52:37 -0400

    funcionalidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'first_app';
+  users = ['john', 'mark', 'will', 'erick', 'peter', 'edward', 'frank', 'robert', 'lawrence', 'brandon', 'zack', 'sam'];
+
+  helloUser (user: string) {
+    alert('hello ' + user);
+  }
+
+  deleteUser (user: string) {
+    var index = this.users.indexOf(user);
+    this.users.splice(index, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 
 // Declarations
 import { AppComponent } from './app.component';
-import { FirstApp } from './first_comp/first.component'
 
 // Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { SecondCompComponent } from './second-comp/second-comp.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstApp,
-    SecondCompComponent
   ],
   imports: [
     BrowserModule,
